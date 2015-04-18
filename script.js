551b20771e625e3132e3e6506b4202cd7f969475
iceburg7 <ecburgess1@gmail.com>
2015-04-18 17:32:18 -0400

    letters in place, starting js

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
 
 	console.log("it works.");
 	
+	var state = 0;
+	
+	
+	
+	
+	
+	
+	
+	var hide = function(y){
+		console.log(y);
+		$(y).removeClass("show");
+		$(y).addClass("hide");
+	}
+	
+	var show = function(y){
+		console.log(y);
+		$(y).removeClass("hide");
+		$(y).addClass("show");
+	}
 	
 	
 	
